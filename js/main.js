7abdf535501bfc8c19fffe63135b2158dd5cee3d
nembo93 <francesco.c193@libero.it>
2022-10-20 16:40:30 +0200

    Aggiunge funzione per visualizzare risultati in documento html

--- js/main.js
+++ js/main.js
@@ -27,12 +27,29 @@ console.log(overDiscount);
 // Applica sconto in base a fascia d'età
 if (userAge < underDiscountAge){
     lastPrice = firstPrice - underDiscount;
-} else if (userAge > overDiscount){
+} else if (userAge > overDiscountAge){
     lastPrice = firstPrice - overDiscount;
 } else{
-    lastPrice = lastPrice - 0;
+    lastPrice = firstPrice - 0;
 }
 lastPrice = lastPrice.toFixed(2);
 console.log(lastPrice);
+underDiscount = underDiscount.toFixed(2);
+overDiscount = overDiscount.toFixed(2);
+
+// Stampa Risultati
+document.getElementById(`user_age`).innerHTML = userAge;
+document.getElementById(`trip_lenght`).innerHTML = tripLenght;
+document.getElementById(`trip_km_cost`).innerHTML = tripCostKm;
+
+if (userAge < underDiscountAge){
+    document.getElementById(`discount`).innerHTML = underDiscount;
+} else if (userAge > overDiscountAge){
+    document.getElementById(`discount`).innerHTML = overDiscount;
+} else {
+    document.getElementById(`discount`).innerHTML = `Nessuno sconto`;
+}
+
+document.getElementById(`last_price`).innerHTML = lastPrice;
 
 
